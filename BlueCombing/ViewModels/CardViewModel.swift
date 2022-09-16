fe9c6569121e1658952e291796867c99cb0a8346
ryu <munam4@naver.com>
2022-09-16 16:40:55 +0900

    [Feat] cardmodel 생

--- BlueCombing/ViewModels/CardViewModel.swift
+++ BlueCombing/ViewModels/CardViewModel.swift
@@ -4,5 +4,10 @@
 //
 //  Created by Wonhyuk Choi on 2022/09/11.
 //
-
+import SwiftUI
 import Foundation
+
+class CardViewModel: ObservableObject {
+    @Published var card = Card(id: 0, distance: 0, time: 0, location: "", backgroundImage: UIImage(named: "xmark")!, badges: [])
+    
+}
