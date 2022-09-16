ae0c5b03b9f38aa71af404387bb9a198e5312598
E-know <eknow25@gmail.com>
2022-09-16 16:38:23 +0900

    [Feat] MainTabView

---
+++ BlueCombing/Model/AppData.swift
@@ -0,0 +1,36 @@
+//
+//  AppData.swift
+//  BlueCombing
+//
+//  Created by Inho Choi on 2022/09/16.
+//
+
+import Foundation
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case label1 = "Label1"
+    case label2 = "Label2"
+    case label3 = "Label3"
+
+    var title: String {
+        rawValue
+    }
+
+    var systemImageName: String {
+        switch self {
+        case .label1: return "banknote"
+        case .label2: return "banknote"
+        case .label3: return "banknote"
+        }
+    }
+
+    @ViewBuilder
+    var view: some View {
+        switch self {
+        case .label1: Text("Hello World")
+        case .label2: Text("Hello World")
+        case .label3: Text("Hello World")
+        }
+    }
+}
