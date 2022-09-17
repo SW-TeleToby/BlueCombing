6152ce58c3d8d1159d1bd5347cb68fd7c77a4083
ryu <munam4@naver.com>
2022-09-17 12:06:44 +0900

    [Feat] 로티 패키지 설정, 뱃지 디테일 뷰 완성

---
+++ BlueCombing/Utills/Lottie.swift
@@ -0,0 +1,46 @@
+//
+//  Lottie.swift
+//  BlueCombing
+//
+//  Created by ryu hyunsun on 2022/09/17.
+//
+
+import Foundation
+import SwiftUI
+import Lottie
+import UIKit
+
+struct LottieView: UIViewRepresentable {
+    var name : String
+    var loopMode: LottieLoopMode
+    
+    init(jsonName: String = "", loopMode : LottieLoopMode = .loop){
+        self.name = jsonName
+        self.loopMode = loopMode
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+
+        let animationView = AnimationView()
+        let animation = Animation.named(name)
+        animationView.animationSpeed = 0.1
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+        animationView.backgroundBehavior = .pauseAndRestore
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(animationView)
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
