3f7b32a05f51282b36f9e51e51b78ff3e163858a
ryu <munam4@naver.com>
2022-10-15 21:40:56 +0900

    [Refactor] 카드 생성 뷰 코드 리팩토링

--- BlueCombing/Views/MakeCardViews/MakeCardView.swift
+++ BlueCombing/Views/MakeCardViews/MakeCardView.swift
@@ -21,7 +21,7 @@ struct MakeCardView: View {
     @State var isPresentShareView = false
     @State var cameraDenyAlert = false
     @State var presentBadge = ""
-    var isCustom: Bool
+    let isCustom: Bool
     var movingDistance : Double
     var movingTime : Int
     var routeImage: Image
@@ -32,133 +32,137 @@ struct MakeCardView: View {
         HStack{
             Image(systemName: "chevron.backward")
                 .font(.system(size: 20, weight: .semibold))
-                .padding(.leading,20)
                 .onTapGesture {
                     dismiss()
                 }
             Spacer()
             Text("비치코밍 카드 만들기")
                 .font(.system(size: 17, weight: .bold))
-                .padding(.leading, -40)
+                .padding(.leading, -20)
             Spacer()
         }.padding(.vertical, 10)
             .padding(.bottom, 10)
     }
     
     var saveImageView: some View {
-        VStack(spacing:0){
             ZStack{
                 CardView(card: $cardViewModel.newCard)
-                routeImage
-                    .resizable()
-                    .frame(width: containerWidth/2, height: containerWidth/2)
-            }.frame(width: containerWidth, height: imageHeight)
-            
-        // .edgesIgnoringSafeArea(.all) 얘를 넣어줘야 위에 여백 안생긴다.
-        }
-        .edgesIgnoringSafeArea(.all)
-    }
-    
-    
-    var body: some View {
-        VStack(spacing:0) {
-            
-            makeCardViewNavbar
-            
-            ZStack{
-                CardView(card: $cardViewModel.newCard)
-                    .frame(width: containerWidth, height: imageHeight)
-                
+                    .frame(height: imageHeight)
                 routeImage
                     .resizable()
                     .scaledToFit()
                     .frame(width: containerWidth/2, height: containerWidth/2)
             }
-            if isCustom {
-                Button(action: {
-                    showingOption = true
-                }){
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.combingGray5)
-                        Text("이미지 변경하기")
-                            .font(.system(size: 16, weight: .semibold))
-                            .foregroundColor(.white)
-                    }.frame(width: containerWidth, height:45)
-                }.confirmationDialog("카드 이미지 변경", isPresented: $showingOption, titleVisibility: .visible) {
-                    Button("카메라 촬영"){
-                        takePicture()
-                    }
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+    var changeImageButton: some View {
+        Button(action: {
+            showingOption = true
+        }){
+            ZStack {
+                Rectangle()
+                    .fill(Color.combingGray5)
+                Text("이미지 변경하기")
+                    .font(.Button1)
+                    .foregroundColor(.white)
+            }.frame(height:45)
+        }.confirmationDialog("카드 이미지 변경", isPresented: $showingOption, titleVisibility: .visible) {
+            Button("카메라 촬영"){
+                takePicture()
+            }
+            
+            Button("앨범에서 사진 선택"){
+                selectImage()
+            }
+            
+            Button("취소", role: .cancel){
+            }
+            
+        }
+    }
+    
+    var completeButton: some View {
+        Button(action: {
+            let saveImage = saveImageView.snapshot()
+            presentBadge = userViewModel.user!.representBadge
+            isPresentShareView.toggle()
+            userViewModel.uploadPicture(image: saveImage)
+            userViewModel.updateUser(uid: userViewModel.user!.id, info: ["total_time": userViewModel.user!.totalTime + movingTime, "total_distance":userViewModel.user!.totalDistance+Int(movingDistance)])
+        }){
+            ZStack {
+                Rectangle()
+                    .fill(Color.combingGradient1)
+                    .frame(height: 56)
+                    .cornerRadius(16)
+                Text("완료")
+                    .font(.Button1)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+    
+    
+    var body: some View {
+        VStack {
+            VStack(spacing:0) {
+                
+                makeCardViewNavbar
+                
+                saveImageView
+
+                // 직접 만들러 가기를 누르면
+                if isCustom {
+                    changeImageButton
                     
-                    Button("앨범에서 사진 선택"){
-                        selectImage()
-                    }
-                    
-                    Button("취소", role: .cancel){
-                    }
-                    
+                    Spacer()
+                    Text("직접 찍은 관광지 사진을 넣어서\n나만의 비치코밍 카드를 만들어보세요!")
+                        .multilineTextAlignment(.center)
+                        .font(.Body4)
+                        .lineSpacing(3)
+                        .foregroundColor(.combingGray4)
                 }
                 
                 Spacer()
-                Text("직접 찍은 관광지 사진을 넣어서\n나만의 비치코밍 카드를 만들어보세요!")
-                    .multilineTextAlignment(.center)
-                    .font(.system(size: 16, weight: .medium))
-                    .lineSpacing(3)
-                    .foregroundColor(Color.combingGray4)
+                completeButton
+
             }
-            
-            Spacer()
-            Button(action: {
-                let saveImage = saveImageView.snapshot()
-                presentBadge = userViewModel.user!.representBadge
-                isPresentShareView.toggle()
-                userViewModel.uploadPicture(image: saveImage)
-                userViewModel.updateUser(uid: userViewModel.user!.id, info: ["total_time": userViewModel.user!.totalTime + movingTime, "total_distance":userViewModel.user!.totalDistance+Int(movingDistance)])
-            }){
-                ZStack {
-                    Rectangle()
-                        .fill(Color.combingGradient1)
-                        .frame(width: containerWidth, height: 56)
-                        .cornerRadius(16)
-                    Text("완료")
-                        .font(.system(size: 16, weight: .semibold))
-                        .foregroundColor(.white)
-                }
-            }
+            .frame(width: containerWidth)
             .navigationTitle("")
             .navigationBarHidden(true)
-        }
-        .sheet(isPresented: $isPresentedAllImage){
-            AllImagePicker(cardViewModel: cardViewModel, card: $cardViewModel.newCard)
-        }
-        .sheet(isPresented: $isPresentedLimitedImage){
-            LimitedImagePicker(cardViewModel: cardViewModel, card: $cardViewModel.newCard)
-        }
-        .sheet(isPresented: $isPresentedCamera) {
-            Camera(cardViewModel: cardViewModel, card: $cardViewModel.newCard)
-        }
-        .alert("이 기능을 사용하려면 카메라 엑세스 권한이 필요합니다", isPresented: $cameraDenyAlert) {
-            Button("나중에 하기") {
-                
+            .sheet(isPresented: $isPresentedAllImage){
+                AllImagePicker(cardViewModel: cardViewModel, card: $cardViewModel.newCard)
             }
-            Button("설정 열기") {
-                if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
-                    UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+            .sheet(isPresented: $isPresentedLimitedImage){
+                LimitedImagePicker(cardViewModel: cardViewModel, card: $cardViewModel.newCard)
+            }
+            .sheet(isPresented: $isPresentedCamera) {
+                Camera(cardViewModel: cardViewModel, card: $cardViewModel.newCard)
+            }
+            .alert("이 기능을 사용하려면 카메라 엑세스 권한이 필요합니다", isPresented: $cameraDenyAlert) {
+                Button("나중에 하기") {
+                    
                 }
+                Button("설정 열기") {
+                    if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+                        UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+                    }
+                }
+            }.sheet(isPresented: $isPresentShareView){
+                ShareView(card: $cardViewModel.newCard, presentBadge: $presentBadge, routeImage: routeImage) {
+                    dismiss()
+                }
+            }.onAppear {
+                if firebaseAuth.currentUser != nil {
+                    userViewModel.getUserData(uid: firebaseAuth.currentUser!.uid)
+                } else {
+                }
+                cardViewModel.checkLocation()
+                cardViewModel.newCard.distance = movingDistance
+                cardViewModel.newCard.time = movingTime
             }
-        }.sheet(isPresented: $isPresentShareView){
-            ShareView(card: $cardViewModel.newCard, presentBadge: $presentBadge, routeImage: routeImage) {
-                dismiss()
-            }
-        }.onAppear {
-            if firebaseAuth.currentUser != nil {
-                userViewModel.getUserData(uid: firebaseAuth.currentUser!.uid)
-            } else {
-            }
-            cardViewModel.checkLocation()
-            cardViewModel.newCard.distance = movingDistance
-            cardViewModel.newCard.time = movingTime
         }
+        
         
     }
     
@@ -176,7 +180,6 @@ struct MakeCardView: View {
                 isPresentedPermissionCheck.toggle()
                 return
             }
-            
         }
     }
     
